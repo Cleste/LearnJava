4a1757e2a5268bfac9eb71aac03a96055db5c576
Kirill Pimenov <kpimen123@gmail.com>
2019-07-13 07:50:22 +0300

    Example with Gallons added

---
+++ src/ch1/GalToLit.java
@@ -0,0 +1,11 @@
+package ch1;
+
+public class GalToLit {
+    public static void main(String[] args)
+    {
+        double gal = 10, lit;
+
+        lit = gal * 3.7854;
+        System.out.println("Десять галлонов равняется " + lit + " литрам");
+    }
+}
