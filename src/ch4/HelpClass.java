f90dd7f62a13d3cd5fc43f7afa53cb92b4c803ba
Kirill Pimenov <kpimen123@gmail.com>
2019-07-14 18:09:06 +0300

    Ex_4.1 added

---
+++ src/ch4/HelpClass.java
@@ -0,0 +1,27 @@
+package ch4;
+
+public class HelpClass {
+    public static void main(String[] args)
+            throws java.io.IOException {
+        char choice, ignore;
+        for (; ; ) {
+            do {
+                Help.showmenu();
+
+                choice = (char) System.in.read();
+
+                do {
+                    ignore = (char) System.in.read();
+                } while (ignore != '\n');
+            } while (!Help.isvalid(choice));
+
+            if(choice == 'q') break;
+
+            System.out.println();
+
+            Help.showmenu(choice);
+
+            System.out.println();
+        }
+    }
+}
